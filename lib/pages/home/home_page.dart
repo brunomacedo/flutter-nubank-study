a46b1ea3e05d9f755a78e51990c8d91cbd27fb33
brunomacedo <contato@brunomacedo.com.br>
2020-09-02 15:22:51 -0300

    feat: add expend icon

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:nubank/pages/home/widgets/my_app_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _showMenu = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +22,9 @@ class HomePage extends StatelessWidget {
       body: Stack(
         alignment: Alignment.topCenter,
         children: <Widget>[
-          MyAppBar(),
+          MyAppBar(
+            showMenu: _showMenu,
+          ),
         ],
       ),
     );
--- lib/pages/home/widgets/my_app_bar.dart
+++ lib/pages/home/widgets/my_app_bar.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
+  final bool showMenu;
+
+  const MyAppBar({Key key, this.showMenu}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -10,33 +14,41 @@ class MyAppBar extends StatelessWidget {
         ),
         Container(
           color: Colors.white10,
-          height: 100,
-          child: Row(
+          height: 200,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              /* Image.network(
-                'https://logodownload.org/wp-content/uploads/2019/08/nubank-logo.png',
-                color: Colors.white,
-                height: 40,
-              ), */
-              Image.asset(
-                'images/logo.png',
-                color: Colors.white,
-                height: 40,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  /* Image.network(
+                    'https://logodownload.org/wp-content/uploads/2019/08/nubank-logo.png',
+                    color: Colors.white,
+                    height: 40,
+                  ), */
+                  Image.asset(
+                    'images/logo.png',
+                    color: Colors.white,
+                    height: 40,
+                  ),
+                  /* SvgPicture.asset(
+                    'images/logo.svg',
+                    height: 35,
+                  ), */
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Bruno',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
               ),
-              /* SvgPicture.asset(
-                'images/logo.svg',
-                height: 35,
-              ), */
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Bruno',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
+              Icon(
+                !showMenu ? Icons.expand_more : Icons.expand_less,
               ),
             ],
           ),
