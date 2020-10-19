01e6932497037fd4d463bd763b7c30187e796f36
brunomacedo <contato@brunomacedo.com.br>
2020-10-19 20:52:47 -0300

    feat(dots): count dynamically items

--- lib/pages/home/widgets/my_custom_dots.dart
+++ lib/pages/home/widgets/my_custom_dots.dart
@@ -3,12 +3,14 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class MyCustomDots extends StatelessWidget {
   final double top;
+  final int counter;
   final controller;
 
   const MyCustomDots({
     Key key,
     this.top,
     this.controller,
+    this.counter,
   }) : super(key: key);
 
   @override
@@ -17,7 +19,7 @@ class MyCustomDots extends StatelessWidget {
       top: top,
       child: SmoothPageIndicator(
         controller: controller,
-        count: 3,
+        count: counter,
         effect: WormEffect(
           spacing: 10,
           dotColor: Colors.white38,
