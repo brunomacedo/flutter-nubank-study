8a7985eee627301f20eb20626a355b94b93e5980
brunomacedo <contato@brunomacedo.com.br>
2020-10-19 20:51:56 -0300

    feat: read list items

--- lib/pages/home/widgets/card_app.dart
+++ lib/pages/home/widgets/card_app.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CardApp extends StatelessWidget {
+  final String text;
+
+  const CardApp({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
       child: Container(
+        padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(5),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.black,
+          ),
         ),
       ),
     );
