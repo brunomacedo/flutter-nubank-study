e7446b84838f560a5def755d4e8d5e5a515aa3eb
brunomacedo <contato@brunomacedo.com.br>
2020-10-19 20:53:31 -0300

    feat: spread and map function examples

--- lib/pages/home/widgets/page_view_app.dart
+++ lib/pages/home/widgets/page_view_app.dart
@@ -5,16 +5,22 @@ class PageViewApp extends StatelessWidget {
   final double top;
   final ValueChanged<int> onChanged;
   final controller;
+  final List<Map<String, Object>> list;
 
   const PageViewApp({
     Key key,
     this.top,
     this.onChanged,
     this.controller,
+    this.list,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> widgets = list.map((item) {
+      return CardApp(text: item['text']);
+    }).toList();
+
     return AnimatedPositioned(
       duration: Duration(milliseconds: 300),
       curve: Curves.easeOut,
@@ -26,10 +32,8 @@ class PageViewApp extends StatelessWidget {
         controller: controller,
         onPageChanged: onChanged,
         physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          CardApp(),
-          CardApp(),
-          CardApp(),
+        children: [
+          ...widgets,
         ],
       ),
     );
